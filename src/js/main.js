dbf0485086af33f112bedccc11f9534e1630e3e6
Serena Voccia <serena.voccia@studentfr.ch>
2024-06-13 22:44:50 +0200

    dot du slider corrigés, slider à finir

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
   slider.addEventListener("animationiteration", checkAnimation);
 });
+// ----------------------
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".carousel-item");
+const dots = document.querySelectorAll(".dot");
+
+function showSlides(n) {
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach((slide) => (slide.style.display = "none"));
+  dots.forEach((dot) => dot.classList.remove("active"));
+
+  slides[slideIndex].style.display = "flex";
+  dots[slideIndex].classList.add("active");
+  updateDotColor(slideIndex); // Appel pour mettre à jour la couleur du dot
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function autoSlides() {
+  slideIndex++;
+  showSlides(slideIndex);
+  setTimeout(autoSlides, 5000); // Change slide every 5 seconds
+}
+
+function updateDotColor(index) {
+  dots.forEach((dot) => dot.classList.remove("dot-1", "dot-2", "dot-3"));
+  dots[index].classList.add(`dot-${index + 1}`);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showSlides(slideIndex);
+  autoSlides();
+});
